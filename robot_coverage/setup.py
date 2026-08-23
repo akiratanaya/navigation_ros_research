a607033797acb337287298c71122c6ba430a131c
akiratanaya <akirakenji78@gmail.com>
2026-08-23 16:06:05 +0700

    feat: run for field boundaries

--- robot_coverage/setup.py
+++ robot_coverage/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'robot_coverage'
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -18,9 +22,11 @@ setup(
     description='ROS 2 Python wrapper for Fields2Cover',
     license='Apache-2.0',
     tests_require=['pytest'],
+    
     entry_points={
         'console_scripts': [
             'coverage_server = robot_coverage.main:main',
+            'field_boundary_collector = robot_coverage.field_boundary_collector:main',
         ],
     },
 )
