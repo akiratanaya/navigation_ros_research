d5cc739e5bd67c26b2e9cca447fe2b5e349670fd
akiratanaya <akirakenji78@gmail.com>
2026-08-23 16:04:59 +0700

    fix:iteration for all celldecomposition

--- robot_coverage/robot_coverage/swath_generator.py
+++ robot_coverage/robot_coverage/swath_generator.py
@@ -7,6 +7,10 @@ class SwathGenerator:
         self.sg_bf = f2c.SG_BruteForce()
 
     def generate(self, no_headland: f2c.Cells, angle: float = math.pi) -> f2c.Swaths:
-        return self.sg_bf.generateSwaths(angle, self.cov_width, no_headland.getGeometry(0))
-
-    
+        all_swaths = f2c.Swaths()
+        for i in range(no_headland.size()):
+            cell_geom = no_headland.getGeometry(i)
+            swaths_i = self.sg_bf.generateSwaths(angle, self.cov_width, cell_geom)
+            for j in range(swaths_i.size()):
+                all_swaths.push_back(swaths_i[j])
+        return all_swaths
